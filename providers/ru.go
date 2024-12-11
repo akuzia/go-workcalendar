0e3e02b7c24e45b0e7555a6ed9d89d84be55277d
Kuzia <kuzia@kuzia.space>
2024-12-11 13:11:00 +0300

    add 2025 ru workcalendar

--- providers/ru.go
+++ providers/ru.go
@@ -5,6 +5,22 @@ import "time"
 func newRUProvider() Provider {
 	return Provider{
 		DaysOff: []time.Time{
+			time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 1, 2, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 1, 3, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 1, 6, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 1, 7, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 1, 8, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 5, 1, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 5, 2, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 5, 8, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 5, 9, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 6, 12, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 6, 13, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 11, 3, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 11, 4, 0, 0, 0, 0, time.Local),
+			time.Date(2025, 12, 31, 0, 0, 0, 0, time.Local),
+
 			time.Date(2024, 1, 1, 0, 0, 0, 0, time.Local),
 			time.Date(2024, 1, 2, 0, 0, 0, 0, time.Local),
 			time.Date(2024, 1, 3, 0, 0, 0, 0, time.Local),
@@ -24,6 +40,8 @@ func newRUProvider() Provider {
 			time.Date(2024, 12, 31, 0, 0, 0, 0, time.Local),
 		},
 		WorkDays: []time.Time{
+			time.Date(2025, 11, 1, 0, 0, 0, 0, time.Local),
+
 			time.Date(2024, 04, 27, 0, 0, 0, 0, time.Local),
 			time.Date(2024, 11, 02, 0, 0, 0, 0, time.Local),
 			time.Date(2024, 12, 28, 0, 0, 0, 0, time.Local),
